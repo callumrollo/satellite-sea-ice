1f05d614420d528615422b1c6b82e2f8f30d30cf
Callum Rollo <c.rollo@outlook.com>
2023-08-09 10:47:36 +0200

    standarise paths and add main function

--- download_ice_data.py
+++ download_ice_data.py
@@ -3,25 +3,33 @@ import requests
 import pandas as pd
 import numpy as np
 from pathlib import Path
-import numpy as np
+images_dir = Path("satellite_images")
+
 
 def get_tif(datestamp, year_dir):
     month_str = datestamp.month_name().lower()[:3]
     date_str = str(datestamp).split(" ")[0]
     year, month, day = date_str.split("-")
     url = f'https://data.seaice.uni-bremen.de/amsr2/asi_daygrid_swath/s3125/{year}/{month_str}/Antarctic3125NoLandMask/asiNOLANDMASK-AMSR2-s3125-{year}{month}{day}-v5.4.tif'
-    print(url)
-    #return
     response = requests.get(url, stream=True)
     if response.status_code == 404:
         return
-    with open(f'images_daily_{year_dir}/{year}-{month}-{day}.tif', 'wb') as out_file:
+    with open(year_dir / f'{year}-{month}-{day}.tif', 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
-    
-for year in np.arange(2015, 2020):
-    if not Path(f"images_daily_{year}").is_dir():
-        Path(f"images_daily_{year}").mkdir()
-    dates = list(pd.date_range(start=f'{year-1}-12-01', end=f'{year}-03-03', freq="1D"))
-    for date in dates:
-        get_tif(date, year)
+
+
+def main():
+    if not images_dir.exists():
+        images_dir.mkdir()
+    for year in np.arange(2015, 2020):
+        year_dir = images_dir / str(year)
+        if not year_dir.exists():
+            year_dir.mkdir()
+        dates = list(pd.date_range(start=f'{year-1}-12-01', end=f'{year}-03-01', freq="1D"))
+        for date in dates:
+            get_tif(date, year_dir)
+
+
+if __name__ == '__main__':
+    main()
